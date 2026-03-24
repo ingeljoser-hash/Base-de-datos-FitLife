35c2f1f236426e84167d8450fd04e65a9e92ab5b
ingeljoser-hash <ingeljoser@gmail.com>
2026-03-23 21:01:35 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿global using Spectre.Console;
+using MiembroCase.Database;
+using MiembroCase.Screens;
+using MiembroCase.Repositories;
+using MiembroCase.Services;
+class Program {
+  public static void Main(string[] args) {
+
+
+  var db = new Database(@"C:\Users\ingel\OneDrive\Desktop\APP_Consola_Fitlife\MiembroCase.db");
+
+
+    MiembroRepository miembroRepository = new(db);
+
+
+    MiembroService miembroService = new(miembroRepository);
+
+
+    MainScreen mainScreen = new(miembroService);
+
+    mainScreen.Show();
+  }
+}
